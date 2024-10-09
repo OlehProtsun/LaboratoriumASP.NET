89b2e349038a6496a5f4a2a15f869de57deb66ed
oleh.protsun1 <olegprocun@gmail.com>
2024-10-09 12:13:34 +0200

    Lab1(09.10.24)End

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using WebApp.Models;
 
 namespace WebApp.Controllers;
@@ -24,8 +25,46 @@ public class HomeController : Controller
     }
     
     //Zadanie 1.
-    public IActionResult Calculator()
+    public IActionResult Calculator(Operator? op, double? x, double? y)
     {
+        //...?op=add&x=4&y=1,5
+        //var op = Request.Query["op"];
+        //var x = double.Parse(Request.Query["x"]);
+        //var y = double.Parse(Request.Query["y"]);
+        if (x is null || y is null)
+        {
+            ViewBag.ErrorMessage = "Niepoprawny format liczby x lub y lub ich brak!";
+            return View("CalculatorEror");
+        }
+
+        if (op is null)
+        {
+            ViewBag.ErrorMessage = "Nieznany operator!";
+            return View("CalculatorEror");
+        }
+        double? result = 0.0;
+        switch (op)
+        {
+            case Operator.Add:
+                result = x + y;
+                ViewBag.Operator = "+";
+                break;
+            case Operator.Div:
+                result = x / y;
+                ViewBag.Operator = "/";
+                break;
+            case Operator.Sub:
+                result = x - y;
+                ViewBag.Operator = "-";
+                break;
+            case Operator.Mul:
+                result = x * y;
+                ViewBag.Operator = "*";
+                break;
+        }
+        ViewBag.Result = result;
+        ViewBag.X = x;
+        ViewBag.Y = y;
         return View();
     }
     
@@ -41,3 +80,10 @@ public class HomeController : Controller
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
+
+public enum Operator
+{
+    Add, Sub, Mul, Div 
+}
+
+// Zdanie domowe: napisz metode Age, ktora przyjmuje paratre z data urodzin i wyswietla wiek w latach miesiacach i dniach. I send link do repoztorium d E.Wsei
